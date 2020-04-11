8385015dd75c9613c8b2ba7ee088937befeca4db
亮亮 <1819345503@qq.com>
2020-04-11 22:58:12 +0800

    高阶函数

---
+++ 博客备份仓库/计算机科技树/python相关/函数式编程/高阶函数.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :高阶函数.py
+# @Time      :2020/4/11 22:43
+# @Author    :亮亮
+
+
+def main():
+
+    f = abs  # 变量名代替函数名
+    print(f(-10))
+
+
+if __name__ == '__main__':
+    main()
